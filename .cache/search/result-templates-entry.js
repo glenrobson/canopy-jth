ed1c8bff40029bcd2382015747c17baacf1b67aa
Glen Robson <glen.robson@gmail.com>
2026-01-28 17:49:36 +0000

    Adding cache of manifests

---
+++ .cache/search/result-templates-entry.js
@@ -0,0 +1,6 @@
+// Auto-generated search result templates map
+import figureTemplate from "/Users/gmr/development/canopy-jth/content/search/_result-figure.mdx";
+export const figure = figureTemplate;
+import articleTemplate from "/Users/gmr/development/canopy-jth/content/search/_result-article.mdx";
+export const article = articleTemplate;
+export default { figure, article };
